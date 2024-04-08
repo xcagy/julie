33dc9cda55bbf52e14c5e207fc1238642e025981
xcagy <159777813+xcagy@users.noreply.github.com>
2024-04-08 21:31:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -212,7 +212,7 @@ function drawText() {
 
     if(frameNumber >= 3000 && frameNumber < 99999){
         context.fillStyle = `rgba(45, 45, 255, ${thirdOpacity})`;
-        context.fillText("Happy Valentine's Day <3", canvas.width/2, (canvas.height/2 + 120));
+        context.fillText("laskuju te<3", canvas.width/2, (canvas.height/2 + 120));
         thirdOpacity = thirdOpacity + 0.01;
 
         button.style.display = "block";
