9dfc4a740370225f71487f477e88a17168ddda12
xcagy <159777813+xcagy@users.noreply.github.com>
2024-04-08 21:52:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -133,12 +133,12 @@ function drawText() {
     }
     if(frameNumber > 1000 && frameNumber < 1250){
         context.fillStyle = `rgba(45, 45, 255, ${opacity})`;
-        context.fillText("jsem rada, ze tomu davame znova sanci, protoze si to jediny co jsem kdy chtela", canvas.width/2, canvas.height/2);
+        context.fillText("jsem rada, mi davas sanci, protoze si to jediny co jsem kdy chtela", canvas.width/2, canvas.height/2);
         opacity = opacity + 0.01;
     }
     if(frameNumber >= 1250 && frameNumber < 1500){
         context.fillStyle = `rgba(45, 45, 255, ${opacity})`;
-        context.fillText("jsem rada, ze tomu davame znova sanci, protoze si to jediny co jsem kdy chtela", canvas.width/2, canvas.height/2);
+        context.fillText("jsem rada, mi davas sanci, protoze si to jediny co jsem kdy chtela", canvas.width/2, canvas.height/2);
         opacity = opacity - 0.01;
     }
 
