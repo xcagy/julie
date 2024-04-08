66adb23e8fc5109af7969e039c874e5361623eb2
xcagy <159777813+xcagy@users.noreply.github.com>
2024-04-08 21:49:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -77,7 +77,7 @@ function drawTextWithLineBreaks(lines, x, y, fontSize, lineHeight) {
 }
 
 function drawText() {
-    var fontSize = Math.min(30, window.innerWidth / 24); // Adjust font size based on screen width
+    var fontSize = Math.min(25, window.innerWidth / 20); // Adjust font size based on screen width
     var lineHeight = 8;
 
     context.font = fontSize + "px Comic Sans MS";
